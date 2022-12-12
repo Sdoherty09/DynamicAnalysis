7f9596a19923fe651c6c7206770a0725eae20437
Shane Doherty <c00249279@itcarlow.ie>
2022-12-12 15:16:54 +0000

    code viewable in gui

--- src/dynamicAnalysis/Window.java
+++ src/dynamicAnalysis/Window.java
@@ -1,40 +1,34 @@
 package dynamicAnalysis;
 
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MenuItem;
-import capstone.*;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.wb.swt.SWTResourceManager;
 
-import com.github.katjahahn.parser.ByteArrayUtil;
-import com.github.katjahahn.parser.IOUtil;
-import com.github.katjahahn.parser.Location;
-import com.github.katjahahn.parser.PEData;
-import com.github.katjahahn.parser.PELoader;
-import com.github.katjahahn.parser.sections.SectionLoader;
-import com.github.katjahahn.parser.sections.rsrc.Resource;
-import com.github.katjahahn.parser.sections.rsrc.ResourceSection;
 
 import org.eclipse.swt.widgets.Table;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.util.List;
 
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
 
 public class Window {
 
 	protected Shell shell;
 	public static byte [] CODE = { 0x55, 0x48, (byte) 0x8b, 0x05, (byte) 0xb8,
 		    0x13, 0x00, 0x00 };
-	private Table table;
+	private Text text;
 
 	/**
 	 * Launch the application.
@@ -70,15 +64,18 @@ public class Window {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(673, 482);
 		shell.setText("SWT Application");
-		shell.setLayout(null);
+		shell.setLayout(new GridLayout(3, false));
 		
 		Menu menu = new Menu(shell, SWT.BAR);
 		shell.setMenuBar(menu);
 		
-		MenuItem mntmFile = new MenuItem(menu, SWT.NONE);
-		mntmFile.setText("File");
+		MenuItem mntmFile_1 = new MenuItem(menu, SWT.CASCADE);
+		mntmFile_1.setText("FIle");
+		
+		Menu menu_1 = new Menu(mntmFile_1);
+		mntmFile_1.setMenu(menu_1);
 		
 		MenuItem mntmEdit = new MenuItem(menu, SWT.NONE);
 		mntmEdit.setText("Edit");
@@ -88,26 +85,39 @@ public class Window {
 		
 		MenuItem mntmHelp = new MenuItem(menu, SWT.NONE);
 		mntmHelp.setText("Help");
-		
-		StyledText styledText = new StyledText(shell, SWT.BORDER);
-		styledText.setBounds(304, 50, 117, 152);
-		
-		Label lblRegistry = new Label(shell, SWT.NONE);
-		lblRegistry.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
-		lblRegistry.setBounds(326, 23, 65, 21);
-		lblRegistry.setText("Registry");
-		
-		StyledText styledText_1 = new StyledText(shell, SWT.BORDER);
-		styledText_1.setBounds(123, 50, 175, 152);
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
 		
 		Label lblCode = new Label(shell, SWT.NONE);
-		lblCode.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.BOLD));
-		lblCode.setBounds(192, 23, 39, 21);
+		lblCode.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1));
+		lblCode.setAlignment(SWT.RIGHT);
+		lblCode.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.BOLD));
 		lblCode.setText("Code");
+		new Label(shell, SWT.NONE);
+		new Label(shell, SWT.NONE);
 		
-		TableViewer tableViewer = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		table = tableViewer.getTable();
-		table.setBounds(10, 10, 93, 218);
+		text = new Text(shell, SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+		text.setEditable(false);
+		GridData gd_text = new GridData(SWT.FILL, SWT.CENTER, false, true, 1, 1);
+		gd_text.widthHint = 194;
+		gd_text.heightHint = 350;
+		text.setLayoutData(gd_text);
 
+		MenuItem mntmOpen = new MenuItem(menu_1, SWT.NONE);
+		mntmOpen.setText("Open");
+		
+		mntmOpen.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event e) {
+            	FileDialog fileDialog = new FileDialog(shell, SWT.MULTI);
+            	String[] files = {
+                        "*.exe",
+                    };
+                    fileDialog.setFilterExtensions(files);
+                    String filePath = fileDialog.open();
+                    CodeExtract codeExtract = new CodeExtract(new File(filePath));
+                    text.setText(codeExtract.getCode());
+                    
+            }
+		});
 	}
 }

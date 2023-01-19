a80cd312d0cba6b25951b68384d15d2121e9444b
Shane Doherty <c00249279@itcarlow.ie>
2023-01-19 14:33:37 +0000

    changed design of GUI

--- src/dynamicAnalysis/Window.java
+++ src/dynamicAnalysis/Window.java
@@ -16,25 +16,20 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
 
 import java.io.File;
-import java.nio.file.Files;
 
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.List;
-import org.eclipse.jface.viewers.ListViewer;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 
 public class Window {
 
 	protected Shell shell;
-	private Table table;
-	private Table table_1;
-	private Table table_2;
+	private Table code;
+	private Table details;
+	private Table dllImports;
 	private String filePath;
 	public static void main(String[] args) {
 		try {
@@ -62,6 +57,7 @@ public class Window {
 	 */
 	protected void createContents() {
 		shell = new Shell();
+		shell.setBackground(SWTResourceManager.getColor(192, 192, 192));
 		shell.setSize(881, 520);
 		shell.setText("SWT Application");
 		GridLayout gl_shell = new GridLayout(9, false);
@@ -103,15 +99,18 @@ public class Window {
 					tableItems[2].setText(1, process.getName());
 					tableItems[3].setText(1, process.getPidAsString());
 					String[] DLLs = process.getDLLs();
-					table_2.clearAll();
-	                table_2.setItemCount(0);
+					dllImports.clearAll();
+	                dllImports.setItemCount(0);
 	                for(int index=0;index<DLLs.length;index++)
 	                {
-	                	TableItem tableItem = new TableItem(table_2, SWT.NULL);
+	                	TableItem tableItem = new TableItem(dllImports, SWT.NULL);
 	                	tableItem.setText(DLLs[index]);
 	                } 
 				}
-				catch(NullPointerException e1) {}
+				catch(NullPointerException e1) 
+				{
+					System.out.println("No file selected");
+				}
 			}
 		});
 		new Label(shell, SWT.NONE);
@@ -124,6 +123,7 @@ public class Window {
 		new Label(shell, SWT.NONE);
 		
 		Label lblCode = new Label(shell, SWT.NONE);
+		lblCode.setBackground(SWTResourceManager.getColor(192, 192, 192));
 		lblCode.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
 		lblCode.setAlignment(SWT.RIGHT);
 		lblCode.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.BOLD));
@@ -133,6 +133,7 @@ public class Window {
 		new Label(shell, SWT.NONE);
 		
 		Label lblDetails = new Label(shell, SWT.NONE);
+		lblDetails.setBackground(SWTResourceManager.getColor(192, 192, 192));
 		GridData gd_lblDetails = new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1);
 		gd_lblDetails.widthHint = 82;
 		lblDetails.setLayoutData(gd_lblDetails);
@@ -142,6 +143,7 @@ public class Window {
 		new Label(shell, SWT.NONE);
 		
 		Label lblDllImports = new Label(shell, SWT.NONE);
+		lblDllImports.setBackground(SWTResourceManager.getColor(192, 192, 192));
 		lblDllImports.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, false, 1, 1));
 		lblDllImports.setText("DLL Imports");
 		lblDllImports.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.BOLD));
@@ -150,41 +152,49 @@ public class Window {
 		new Label(shell, SWT.NONE);
 		
 		TableViewer tableViewer = new TableViewer(shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL);
-		table = tableViewer.getTable();
-		GridData gd_table = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
-		gd_table.widthHint = 121;
-		gd_table.heightHint = 313;
-		table.setLayoutData(gd_table);
+		code = tableViewer.getTable();
+		code.setToolTipText("x86 instructions");
+		code.setBackground(SWTResourceManager.getColor(192, 192, 192));
+		code.setHeaderBackground(SWTResourceManager.getColor(192, 192, 192));
+		GridData gd_code = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		gd_code.widthHint = 121;
+		gd_code.heightHint = 313;
+		code.setLayoutData(gd_code);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		
-		table_1 = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		GridData gd_table_1 = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1);
-		gd_table_1.widthHint = 316;
-		table_1.setLayoutData(gd_table_1);
-		table_1.setHeaderVisible(true);
-		table_1.setLinesVisible(true);
+		details = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		details.setToolTipText("File information");
+		details.setHeaderBackground(SWTResourceManager.getColor(192, 192, 192));
+		details.setBackground(SWTResourceManager.getColor(192, 192, 192));
+		GridData gd_details = new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1);
+		gd_details.widthHint = 316;
+		details.setLayoutData(gd_details);
+		details.setHeaderVisible(true);
+		details.pack();
 		
-		TableColumn tblclmnNewColumn = new TableColumn(table_1, SWT.CENTER);
-		tblclmnNewColumn.setWidth(160);
-		tblclmnNewColumn.setText("Labels");
+		TableColumn labels = new TableColumn(details, SWT.CENTER);
+		labels.setWidth(160);
+		labels.setText("Labels");
 		
-		TableColumn tblclmnNewColumn_1 = new TableColumn(table_1, SWT.CENTER | SWT.V_SCROLL);
-		tblclmnNewColumn_1.setWidth(170);
-		tblclmnNewColumn_1.setText("Values");
+		TableColumn values = new TableColumn(details, SWT.CENTER | SWT.V_SCROLL);
+		values.setWidth(170);
+		values.setText("Values");
 		new Label(shell, SWT.NONE);
 		
-		table_2 = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL);
-		GridData gd_table_2 = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1);
-		gd_table_2.widthHint = 200;
-		table_2.setLayoutData(gd_table_2);
-		table_2.setHeaderVisible(true);
-		table_2.setLinesVisible(true);
+		dllImports = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION | SWT.V_SCROLL);
+		dllImports.setToolTipText("DLL file paths");
+		dllImports.setHeaderBackground(SWTResourceManager.getColor(192, 192, 192));
+		dllImports.setBackground(SWTResourceManager.getColor(192, 192, 192));
+		GridData gd_dllImports = new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1);
+		gd_dllImports.widthHint = 200;
+		dllImports.setLayoutData(gd_dllImports);
+		new Label(shell, SWT.NONE);
 		
 		for(int index = 0;index<tableItems.length;index++)
 		{
-			tableItems[index] = new TableItem(table_1, SWT.NONE);
+			tableItems[index] = new TableItem(details, SWT.NONE);
 		}
 		tableItems[0].setText(0, "Directory");
 		tableItems[1].setText(0, "Version");
@@ -200,37 +210,45 @@ public class Window {
                     };
                     fileDialog.setFilterExtensions(files);
                     filePath = fileDialog.open();
-                    try
+                    tableItems[0].setText(1, filePath);
+                   /* new Thread(new Runnable()
                     {
-                    	CodeExtract codeExtract = new CodeExtract(new File(filePath));
-                        String[] codeArr = codeExtract.getCodeArr();
-                        byte[] resources = codeExtract.getResources();
-                        
-                        tableItems[0].setText(1, filePath);
-                        
-                        if(codeExtract.getPeFile().isX32())
-                        {
-                        	tableItems[1].setText(1, "32-bit");
-                        }
-                        else
-                        {
-                        	tableItems[1].setText(1, "64-bit");
-                        }
-                        
-                        table.clearAll();
-                        table.setItemCount(0);
-                        table_2.clearAll();
-                        for(int index=0;index<codeArr.length;index++)
-                        {
-                        	TableItem tableItem = new TableItem(table, SWT.NULL);
-                        	tableItem.setText(codeArr[index]);
-                        }
-                        
-                    }
-                    catch (NullPointerException e1)
-                    {
-                    	e1.printStackTrace();
-                    }
+                        @Override
+                        public void run()
+                        {*/
+                        	try
+                            {
+                            	CodeExtract codeExtract = new CodeExtract(new File(filePath));
+                                String[] codeArr = codeExtract.getCodeArr();
+                               // byte[] resources = codeExtract.getResources();
+                                
+                                
+                                
+                                if(codeExtract.getPeFile().isX32())
+                                {
+                                	tableItems[1].setText(1, "32-bit");
+                                }
+                                else
+                                {
+                                	tableItems[1].setText(1, "64-bit");
+                                }
+                                
+                                code.clearAll();
+                                code.setItemCount(0);
+                                dllImports.clearAll();
+                                for(int index=0;index<codeArr.length;index++)
+                                {
+                                	TableItem tableItem = new TableItem(code, SWT.NULL);
+                                	tableItem.setText(codeArr[index]);
+                                }
+                                
+                            }
+                            catch (NullPointerException e1)
+                            {
+                            	System.out.println("Explorer closed");
+                            }
+                       /* }
+                    }).start();*/
             }
 		});
 	}
